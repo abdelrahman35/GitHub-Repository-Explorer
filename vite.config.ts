9899313a4c19b7c5ac9b12aa139dbe4295bb0cde
abdelrahman tarek <abdelrahman.tarek.omar@gmail.com>
2025-02-16 19:39:50 +0200

    update: remove tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 import path from "node:path";
 
@@ -8,7 +7,7 @@ import path from "node:path";
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+
     svgr({
       svgrOptions: {
         exportType: "named",
